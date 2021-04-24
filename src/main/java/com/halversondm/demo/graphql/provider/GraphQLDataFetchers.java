08b8bb6e5593aa883674f21baa9f412c6b8698d6
Dan Halverson <daniel.m.halverson@gmail.com>
2021-04-24 11:50:55 -0500

    database implemented

--- src/main/java/com/halversondm/demo/graphql/provider/GraphQLDataFetchers.java
+++ src/main/java/com/halversondm/demo/graphql/provider/GraphQLDataFetchers.java
@@ -1,63 +1,33 @@
 package com.halversondm.demo.graphql.provider;
 
-import com.google.common.collect.ImmutableMap;
+import com.halversondm.demo.graphql.entity.Author;
+import com.halversondm.demo.graphql.entity.Book;
+import com.halversondm.demo.graphql.repository.AuthorRepository;
+import com.halversondm.demo.graphql.repository.BookRepository;
 import graphql.schema.DataFetcher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 
 @Component
 public class GraphQLDataFetchers {
 
-    private static List<Map<String, String>> books = Arrays.asList(
-            ImmutableMap.of("id", "book-1",
-                    "name", "Harry Potter and the Philosopher's Stone",
-                    "pageCount", "223",
-                    "authorId", "author-1"),
-            ImmutableMap.of("id", "book-2",
-                    "name", "Moby Dick",
-                    "pageCount", "635",
-                    "authorId", "author-2"),
-            ImmutableMap.of("id", "book-3",
-                    "name", "Interview with the vampire",
-                    "pageCount", "371",
-                    "authorId", "author-3")
-    );
+    @Autowired
+    AuthorRepository authorRepository;
 
-    private static List<Map<String, String>> authors = Arrays.asList(
-            ImmutableMap.of("id", "author-1",
-                    "firstName", "Joanne",
-                    "lastName", "Rowling"),
-            ImmutableMap.of("id", "author-2",
-                    "firstName", "Herman",
-                    "lastName", "Melville"),
-            ImmutableMap.of("id", "author-3",
-                    "firstName", "Anne",
-                    "lastName", "Rice")
-    );
+    @Autowired
+    BookRepository bookRepository;
 
-    public DataFetcher getBookByIdDataFetcher() {
+    public DataFetcher<Book> getBookByIdDataFetcher() {
         return dataFetchingEnvironment -> {
             String bookId = dataFetchingEnvironment.getArgument("id");
-            return books
-                    .stream()
-                    .filter(book -> book.get("id").equals(bookId))
-                    .findFirst()
-                    .orElse(null);
+            return bookRepository.findById(bookId).orElse(null);
         };
     }
 
-    public DataFetcher getAuthorDataFetcher() {
+    public DataFetcher<Author> getAuthorDataFetcher() {
         return dataFetchingEnvironment -> {
-            Map<String,String> book = dataFetchingEnvironment.getSource();
-            String authorId = book.get("authorId");
-            return authors
-                    .stream()
-                    .filter(author -> author.get("id").equals(authorId))
-                    .findFirst()
-                    .orElse(null);
+            Book book = dataFetchingEnvironment.getSource();
+            return authorRepository.findById(book.getAuthorId()).orElse(null);
         };
     }
 }
